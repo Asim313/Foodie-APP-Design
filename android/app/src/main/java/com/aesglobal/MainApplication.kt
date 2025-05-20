a71f89932666a985fc16f8cbb3cc7c3c68ebd12c
Asim313 <graysonrivas818@gmail.com>
2025-05-20 17:55:21 +0500

    updated

--- android/app/src/main/java/com/aesglobal/MainApplication.kt
+++ android/app/src/main/java/com/aesglobal/MainApplication.kt
@@ -1,14 +1,16 @@
 package com.aesgloballite
 
 import android.app.Application
+import android.content.Context
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
+import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.ReactHost
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
@@ -17,7 +19,7 @@ class MainApplication : Application(), ReactApplication {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
+              // Manually add packages that aren't autolinked here
               // add(MyReactNativePackage())
             }
 
@@ -36,8 +38,24 @@ class MainApplication : Application(), ReactApplication {
     super.onCreate()
     SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
+    }
+
+    initializeFlipper(this, reactNativeHost.reactInstanceManager)
+  }
+
+  /**
+   * Loads Flipper in debug builds. Uses reflection to avoid bundling in release builds.
+   */
+  private fun initializeFlipper(context: Context, reactInstanceManager: ReactInstanceManager) {
+    if (BuildConfig.DEBUG) {
+      try {
+        val clazz = Class.forName("com.aesgloballite.ReactNativeFlipper")
+        val method = clazz.getMethod("initializeFlipper", Context::class.java, ReactInstanceManager::class.java)
+        method.invoke(null, context, reactInstanceManager)
+      } catch (e: Exception) {
+        e.printStackTrace()
+      }
     }
   }
 }
